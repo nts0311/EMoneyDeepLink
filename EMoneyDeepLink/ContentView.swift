b186180d377f9e4cc234398531a37ee9fd886e4a
sonnt <sonnt@biplus.com.vn>
2023-03-15 15:10:44 +0700

    create

--- EMoneyDeepLink/ContentView.swift
+++ EMoneyDeepLink/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var msisdn: String = ""
+    @State var partnerCode: String = ""
+    @State var accountPartnerId: String = ""
+    
+    var deepLink: String {
+        return "eMoneyEU://link_passapp?msisdn=\(msisdn)&partnerCode=\(partnerCode)&accountPartnerId=\(accountPartnerId)"
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Form {
+            Section {
+                TextField("msisdn", text: $msisdn)
+                TextField("partnerCode", text: $partnerCode)
+                TextField("msisdn", text: $accountPartnerId)
+            } header: {
+                Text("Deeplink Inputs")
+            }
+            
+            Section {
+                Text(deepLink)
+            } header: {
+                Text("Deeplink preview")
+            }
+            
+            HStack {
+                Button("To Emoney", action: invokeEMoneyDeepLink)
+                    .buttonStyle(.automatic)
+                    .frame(maxWidth: .infinity)
+            }
         }
-        .padding()
+        
+    }
+    
+    func invokeEMoneyDeepLink() {
+        
+        guard let deepLinkUrl = URL(string: deepLink) else {
+            openAppStore()
+            return
+        }
+        
+        let isEMoneyEUInstalled = UIApplication.shared.canOpenURL(URL(string: "eMoneyEU://")!)
+        
+        if isEMoneyEUInstalled {
+            UIApplication.shared.open(deepLinkUrl)
+        } else {
+            openAppStore()
+        }
+    }
+    
+    func openAppStore() {
+        UIApplication.shared.open(URL(string: "https://apps.apple.com/app/emoney-end-user/id1069866065")!)
     }
 }
 
